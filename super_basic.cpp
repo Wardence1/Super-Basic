179213eb15f7107bf54591078917c01a2c572ec8
Jackson Kidwell <130793828+Wardence1@users.noreply.github.com>
2025-02-07 15:02:57 -0500

    Small Changes

--- super_basic.cpp
+++ super_basic.cpp
@@ -12,8 +12,9 @@ vector<string> command, arg1, arg2;
 bool carryFlag = false;
 
 // Returns an entire string if quotes are used
-// Strings return with a quotation mark at [0]
 string getElement(istringstream &iss, string start, unsigned lineNum) {
+    // NOTE: Strings return with a quotation mark at [0]
+
     if (start.find("\"") != string::npos) {
         string word, str = start;
 
@@ -59,6 +60,9 @@ string parseString(string str, unsigned lineNum) {
                 case 't':
                     temp.push_back('\t');
                     break;
+                case 'r':
+                    temp.push_back('\r');
+                    break;
                 case '\\':
                     temp.push_back('\\');
                     break;
@@ -81,15 +85,24 @@ bool isNum(string str) {
     return true;
 }
 
-bool jump(unsigned& token) {
+bool setLower(string& str) {
+
+    for (int i = 0; i < str.size(); i++) {
+        str[i] = tolower(str[i]);
+    }
+    return true;
+}
+
+bool jump(unsigned& token, string curLabel) {
+    setLower(curLabel);
     bool end = false;
     for (auto label : labels) {
-        if (label.first == arg1[token]) {
+        if (label.first == curLabel) {
             token = label.second+1;
             return true;
         }
     }
-    fprintf(stderr, "Error: %s is not a registered label.\n", arg1[token].c_str());
+    fprintf(stderr, "Error: %s is not a registered label.\n", curLabel.c_str());
     exit(1);
 }
 
@@ -101,6 +114,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    /* Read the File */
     ifstream file(argv[1]);
     if (!file) {
         fprintf(stderr, "Couldn't read file: \"%s\"\n", argv[1]);
@@ -158,60 +172,41 @@ int main(int argc, char** argv) {
         }
     }
 
-    /* Commmand Declarations */
+    /* Command Declarations */
     for (int i = 0; i < command.size(); i++) {
         
         if (command[i][command[i].find_last_not_of(' ')] == ':') {
+            setLower(command[i]);
             labels.push_back({command[i].substr(0, command[i].find_last_not_of(' ')), i});
         }
     }
 
     /* Go Through Script */
-    // Note: token does not represent line count, commented/null lines are exluded during tokenization.
+    // NOTE: token does not represent line count, commented/null lines are excluded during tokenization.
     unsigned token = 0; // Current command of the script
+
+    jump(token, "start"); // Start at the start label
 
     while (token < command.size()) {
 
-        if (command[token] == "print") {
+        /* Output */
+        if (command[token] == "print") { // Print a string
             cout << arg1[token].substr(1);
-        } else if (command[token] == "prints") {
+        } else if (command[token] == "prints") { // Print the top of the string stack
             if (strStk.empty()) { fprintf(stderr, "Error, command \"prints\": Can't print from an empty stack.\n"); exit(-1); }
             cout << strStk.front();
             if (arg1[token] != "") cout << arg1[token].substr(1);
-        } else if (command[token] == "printn") {
+        } else if (command[token] == "printn") { // Print the top of the number stack
             if (numStk.empty()) { fprintf(stderr, "Error, command \"printn\": Can't print from an empty stack.\n"); exit(-1); };
             cout << numStk.front();
             if (arg1[token] != "")cout << arg1[token].substr(1);
 
-        } else if (command[token] == "push") {
-            arg1[token][0] == '\"' ? strStk.push_back(arg1[token].substr(1)) : numStk.push_back(stoi(arg1[token]));
-        } else if (command[token] == "pops") {
-            if (strStk.empty()) { fprintf(stderr, "Error, command \"pops\": Can't pop from an empty stack.\n"); exit(-1); };
-            strStk.pop_back();
-        } else if (command[token] == "popn") {
-            if (numStk.empty()) { fprintf(stderr, "Error, command \"popn\": Can't pop from an empty stack.\n"); exit(-1); };
-            numStk.pop_back();
-
-        } else if (command[token] == "add") {
-            if (isNum(arg1[token])) {
-                numStk[numStk.size()-1] += stoi(arg1[token]);
-            } else {
-                fprintf(stderr, "Error, The first arg of \"add\" is a number.");
-                return 1;
-            }
-        } else if (command[token] == "sub") {
-            if (isNum(arg1[token])) {
-                numStk[numStk.size()-1] -= stoi(arg1[token]);
-            } else {
-                fprintf(stderr, "Error, The first arg of \"sub\" is a number.");
-                return 1;
-            }
-
-        } else if (command[token] == "inputs") {
+        /* Input */
+        } else if (command[token] == "inputs") { // Takes in a string as input from the user
             string input;
             getline(cin, input);
             strStk.push_back(input);
-        } else if (command[token] == "inputn") {
+        } else if (command[token] == "inputn") { // Takes in a number as input from the user
             string input;
             getline(cin, input);
             if (input == "") input = "0";
@@ -222,7 +217,35 @@ int main(int argc, char** argv) {
                 return 1;
             }
 
-        } else if (command[token] == "cmp") {
+        /* Stack Operations */
+        } else if (command[token] == "push") { // Pushes a string or number onto the stack, the stack is decided dynamically
+            arg1[token][0] == '\"' ? strStk.push_back(arg1[token].substr(1)) : numStk.push_back(stoi(arg1[token]));
+        } else if (command[token] == "pops") { // Pops the top of the string stack
+            if (strStk.empty()) { fprintf(stderr, "Error, command \"pops\": Can't pop from an empty stack.\n"); exit(-1); };
+            strStk.pop_back();
+        } else if (command[token] == "popn") { // Pops the top of the number stack
+            if (numStk.empty()) { fprintf(stderr, "Error, command \"popn\": Can't pop from an empty stack.\n"); exit(-1); };
+            numStk.pop_back();
+
+        /* Number Manipulation */
+        } else if (command[token] == "add") { // Adds arg1 to the number at the top of the number stack
+            if (isNum(arg1[token])) {
+                numStk[numStk.size()-1] += stoi(arg1[token]);
+            } else {
+                fprintf(stderr, "Error, The first arg of \"add\" is a number.");
+                return 1;
+            }
+        } else if (command[token] == "sub") { // Subtracts arg1 to the number at the top of the number stack
+            if (isNum(arg1[token])) {
+                numStk[numStk.size()-1] -= stoi(arg1[token]);
+            } else {
+                fprintf(stderr, "Error, The first arg of \"sub\" is a number.");
+                return 1;
+            }
+
+
+        /* Comparing */
+        } else if (command[token] == "cmp") { // arg1 is a symbol while arg2 is the number being compared to the top of the number stack with the symbol, the carry flag is set accordingly
             
             if (arg2[token] == "" || !isNum(arg2[token])) {
                 fprintf(stderr, "Error: arg2 of \"cmp\" needs to contain a number.\n");
@@ -240,18 +263,32 @@ int main(int argc, char** argv) {
                 carryFlag = numStk.front() < num;
             }
 
-        } else if (command[token] == "jmp") {
+        /* Control Flow */
+        } else if (command[token] == "jmp") { // Jumps to the label specified at arg1
             
-            jump(token);
+            jump(token, arg1[token]);
             continue;
 
-        } else if (command[token] == "jmpi") {
+        } else if (command[token] == "jmpt") { // Jumps to the label specified at arg1 if the carry flag is set to true
         
             if (carryFlag) {
                 
-                jump(token);
+                jump(token, arg1[token]);
                 continue;
             }
+
+        } else if (command[token] == "jmpf") { // Jumps to the label specified at arg1 if the carry flag is set to false
+        
+            if (!carryFlag) {
+                
+                jump(token, arg1[token]);
+                continue;
+            }
+
+        /* System Commands */
+        } else if (command[token] == "exit") { // Causes the program returns zero
+            return 0;
+
 
         } else {
 
@@ -262,6 +299,7 @@ int main(int argc, char** argv) {
             }
             if (end) break;
 
+            // Invalid command
             fprintf(stderr, "Invalid command: \"%s\"\n", command[token].c_str());
             exit(1);
         }
